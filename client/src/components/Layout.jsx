e4fbe40d122c5889897627a7ca42906290d4e6ec
georgetheoracle <uche1694@student.babcock.edu.ng>
2023-09-18 18:45:57 +0100

    update

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { useState } from 'react'
+import './App.css'
+import Header from './Header'
+import Sidebar from './Sidebar'
+import Home from './Home'
 
 function Layout() {
-  return (
-    <div>Layout</div>
+    const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
+
+    const OpenSidebar = () => {
+    setOpenSidebarToggle(!openSidebarToggle)
+        }
+    return (
+
+    <div className='grid-container'>
+      <Header OpenSidebar={OpenSidebar}/>
+      <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
+      <Home />
+    </div>
   )
 }
 
